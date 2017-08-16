dec393bf352ed6c695c70b64a3cf465a70b00f86
Bassab03 <21043977+Bassab03@users.noreply.github.com>
2017-08-16 20:50:49 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,34 @@
+# Mathbot
+A Discord bot that does simple math.
+
+Mathbot requires you to host your own instance, since I currently don't have the money to host it on a webserver 24/7.
+
+##Requirements
+* node.js (v6.11.2 or later recommended)
+
+##Installation
+1. Go to `https://discordapp.com/developers` and click 'My Apps'.
+2. Create a new app by clicking on "New App".
+3. Name it whatever you like, but I think Mathbot sounds cool :D
+4. Give it a description and profile picture, you know the drill.
+5. Press "Create App".
+6. Now go back to the GitHub repository and click 'Clone or download' and then 'Download ZIP'.
+7. Go into your file explorer, unZIP the file somewhere on your computer and go into the folder.
+8. Next, open `config.json` in a plain text editor.
+9. Go back to the page of your app on `https://discordapp.com/developers`.
+10. Copy the number next to 'Client ID' and paste it over `ClientIDGoesHere` in `config.json`.
+11. Go back to the app page, reveal the token and paste it over `BotTokenGoesHere` in `config.json`.
+12. Ignore `"prefix"` for now. Save `config.json` and close it.
+13. Copy the file path of the folder that contains `bot.js` and `config.json`.
+14. Press Win+R and type in `cmd`, then hit Enter.
+15. In the command line, type in `cd ` and paste in the file path you copied, then hit Enter.
+16. Now type `npm install discord.js` and hit Enter.
+17. Type `node bot.js` and hit Enter. If you executed the previous steps successfully, the console will say `Joined all available servers.`, followed by a link!
+19. Copy the link and paste it into your web browser's URL bar.
+20. Authorize the bot on your Discord account and select the server you want it to join, then click 'Authorize'. (note: You need to have 'Manage Server' permissions on a server if you want to let a bot in)
+21. The bot will now join the server! Enjoy!
+
+##Bugs and issues
+If you happen to find anything you believe is an issue, you can do one of the following:
+* Join the official Mathbot Help Chat at https://discord.gg/n27yKQN.
+* Create an issue on Mathbot's GitHub.
---
+++ bot.js
@@ -0,0 +1,53 @@
+const Discord = require("discord.js");
+const client = new Discord.Client();
+const fs = require("fs");
+const config = require("./config.json");
+
+client.login(config.token);
+
+client.on("ready", () => {
+  console.log("Joined all available servers.\nhttps://discordapp.com/oauth2/authorize?client_id=" + config.id + "&scope=bot");
+  client.user.setGame("with numbers");
+});
+
+function getSubstringIndex(str, substring, n) {
+    var times = 0, index = null;
+
+    while (times < n && index !== -1) {
+        index = str.indexOf(substring, index+substring.length);
+        times++;
+    }
+
+    return index;
+}
+
+client.on("message", (message) => {
+  if ((!message.content.startsWith(config.prefix) && !message.content.toLowerCase().startsWith("math.")) || message.author.bot) return;
+  if (message.content.startsWith("math.ping")) {
+    message.channel.send("Pong!");
+  } else if (message.content.startsWith("math.invite")) {
+    message.channel.send("https://discordapp.com/oauth2/authorize?client_id=" + config.id + "&scope=bot");
+  } else if (message.content.startsWith("math.server")) {
+    message.channel.send("https://discord.gg/n27yKQN");
+  } else if (message.content.toLowerCase().startsWith("math.help")) {
+    message.channel.send("```math.help     Shows this information.\nmath.ping     Pings me.\nmath.invite   Invites me to your server.\nmath.server   Invites you to my server.\n\n" + config.prefix + "             Prefix.\n+             Add.\n-             Subtract.\n*             Multiply.\n/             Divide.\n%             Modulo.\n\npow(x, y)     Raises x to the power of y.\nsqrt(x)       Square root of x.\nfloor(x)      Rounds x down to nearest integer.\nceil(x)       Rounds x up to nearest integer.```");
+  } else if (message.content.startsWith(config.prefix)) {
+     if (isFinite(message.content.toLowerCase().replace(/\=|\+|\-|\*|\/|\%|\(|\)|\,|\ |math.|pow|sqrt|round|floor|ceil|ceiling|pi|π|e|euler|abs|absolute|exp|log|logarithm|random|rand|rng/g,''))) {
+      let calculate = message.content.toLowerCase().replace(/ /g, "").replace(/pow|power/g, "Math.pow").replace(/sqrt|squareroot/g, "Math.sqrt").replace(/round/g, "Math.round").replace(/floor/g, "Math.floor").replace(/ceil|ceiling/g, "Math.ceil").replace(/pi|π/g, "Math.PI").replace(/e|euler/g, "Math.E").replace(/abs|absolute/g, "Math.abs").replace(/exp/g, "Math.exp").replace(/log|logarithm/g, "Math.log").replace(/random|rand|rng/g, "Math.random()");/*.replace(/acos|arccosine/g, "Math.acos").replace(/asin|arcsine/g, "Math.asin").replace(/atan|arctangent|atan1|arctangent1/g, "Math.atan").replace(/atan2|arctangent2/g, "Math.atan2").replace(/cos|cosine/g, "Math.cos").replace(/sin|sine/g, "Math.sin").replace(/tan|tangent/g, "Math.tan")*/;
+      if (calculate.replace(/[^%]/g, "").length > 0) {
+        for (let i = 0; i < calculate.replace(/[^%]/g, "").length; i++) {
+          while ((calculate[getSubstringIndex(calculate, "%", i+1) + 1] == "+" || calculate[getSubstringIndex(calculate, "%", i+1) + 1] == "-" || calculate[getSubstringIndex(calculate, "%", i+1) + 1] == "*" || calculate[getSubstringIndex(calculate, "%", i+1) + 1] == "/" || calculate[getSubstringIndex(calculate, "%", i+1) + 1] == "(" || calculate[getSubstringIndex(calculate, "%", i+1) + 1] == ")" || calculate[getSubstringIndex(calculate, "%", i+1) + 1] == "," || getSubstringIndex(calculate, "%", i+1) + 1 == calculate.length) && calculate.replace(/[^%]/g, "").length > 0) {
+            for (let j = getSubstringIndex(calculate, "%", i+1); j > -1; j--) {
+              if (calculate[j] == "=" || calculate[j] == "+" || calculate[j] == "-" || calculate[j] == "*" || calculate[j] == "/" || calculate[j] == "(" || calculate[j] == ")" || calculate[j] == ",") {
+                calculate = calculate.substring(0, j+1) + (calculate.substring(j+1, getSubstringIndex(calculate, "%", i+1))/100) + calculate.substring(getSubstringIndex(calculate, "%", i+1)+1, calculate.length);
+                break;
+              }
+            }
+          }
+        }
+      }
+      calculate =  calculate.replace(/=/g, "");
+      if (isFinite(eval(calculate))) message.channel.send(eval(calculate));
+    }
+  }
+});
